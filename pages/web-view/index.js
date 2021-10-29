ba7608b084f192f6370e872a1943e38a83ab56a6
ShadowWalker <master@rebeta.cn>
2021-10-29 17:48:18 +0800

    feat: 兼容带参数的链接;

--- pages/web-view/index.js
+++ pages/web-view/index.js
@@ -5,16 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-    src: ""
+    src: "",
+    suffix: {}, // 推广后缀
+    suffixStr: '', // 推广后缀字符串
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    this.setData({
-      src: options.src
-    })
+  onLoad: async function (options) {
+    this.setData(await getApp().methods.getSuffix(options)); // 获取后缀信息
+ 
+    // 将页面收到的参数和后缀信息合并
+    let currentSuffix = {...this.data.suffix, ...options};
+    delete currentSuffix.src;
+
+    // 把参数对象转换为字符串
+    let currentSuffixStr = '';
+    for (let key in currentSuffix) {
+      currentSuffixStr += `${key}=${currentSuffix[key]}&`;
+    }
+    currentSuffixStr = currentSuffixStr.substr(0, currentSuffixStr.length - 1); // 裁剪最后一个 &
+
+    if (options.src) {
+      this.setData({ src: `${options.src}${currentSuffixStr ? `?${currentSuffixStr}` : ''}` })
+    } else {
+      this.setData({ src: "https://www.offcn.com" })
+    }
+    
   },
 
   /**
